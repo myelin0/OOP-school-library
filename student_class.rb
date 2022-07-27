2b0c0415edafcab074ec174ed60b0758df770c1b
myelin0 <emi.veisllari@gmail.com>
2022-07-27 22:19:17 +0200

    Add teacher class

--- student_class.rb
+++ student_class.rb
@@ -1,4 +1,4 @@
-require "./person_class.rb"
+require './person_class'
 class Student < Person
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(name, age, parent_permission)
