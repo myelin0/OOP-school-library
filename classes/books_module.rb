3449e7aa7c36890d2d240d969f28fd01e1a96236
Ghilain <ishimweghilain5050@gmail.com>
2022-08-03 14:47:20 +0200

    Add book module

---
+++ classes/books_module.rb
@@ -0,0 +1,30 @@
+require './book'
+
+class BookModule
+  attr_accessor :books
+
+  def initialize(books)
+    @books = books
+  end
+
+  def list_all_books
+    puts 'No books available at the moment' if books.empty?
+    @books.each { |book| puts "Title: \"#{book.title}\", Author: #{book.author}" }
+    end
+  end
+
+  def create_book
+    print 'Title: '
+    title = gets.chomp
+    print 'Author: '
+    author = gets.chomp
+
+    if title.strip != '' && author.strip != ''
+      book = Book.new(title, author)
+      @books << book
+      puts 'Book created successfully'
+    else
+      puts 'Please enter title and author to create a book'
+    end
+  end
+end
